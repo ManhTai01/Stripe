eb0ed9f7cc5160cb2ea3d3509c07b79d11ea0aa5
Manh Tai <lemanhtaictbp@gmail.com>
2020-12-30 15:53:28 +0700

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
+const Navbar = () => {
+  const { openSibebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.gerBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom });
+  };
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("link-btn")) {
+      closeSubmenu();
+    }
+  };
+  return (
+    <nav className="nav" onMouseOver={handleSubmenu}>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="stripe" className="nav-logo"></img>
+          <button className="btn toggle-btn" onClick={openSibebar}>
+            <FaBars></FaBars>
+          </button>
+        </div>
+        <ul>
+          <li className="links">
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign In</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
